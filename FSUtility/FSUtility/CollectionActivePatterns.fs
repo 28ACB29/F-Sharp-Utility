288e2ccb1f0dcecbaa73932f1937563ae064bfb8
Arthur Baroi <arthurcbaroi@hotmail.com>
2020-03-08 23:06:14 -0500

    Long Overdue Commit
    Coming back to this after a long time. Added Active Patterns for collections and updated the String module.

---
+++ FSUtility/FSUtility/CollectionActivePatterns.fs
@@ -0,0 +1,307 @@
+﻿namespace FSUtility
+
+open System
+
+module CollectionActivePatterns =
+
+    let private BoolToOption (result:bool) =
+        match result with
+        | true -> Some()
+        | false -> None
+
+    let (|ArrayContains|_|) (value:'a) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.contains value
+            |> BoolToOption
+
+    let (|ArrayExactlyOne|_|) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            try
+                source
+                |> Array.exactlyOne
+                |> Some
+            with
+                | _ -> None
+
+    let (|ArrayExists|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.exists predicate
+            |> BoolToOption
+
+    let (|ArrayFind|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryFind predicate
+
+    let (|ArrayFindBack|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryFindBack predicate
+
+    let (|ArrayFindIndex|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryFindIndex predicate
+
+    let (|ArrayFindIndexBack|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryFindIndexBack predicate
+
+    let (|ArrayForAll|_|) (predicate:'a -> bool) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.forall predicate
+            |> BoolToOption
+
+    let (|ArrayHead|_|) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryHead
+
+    let (|ArrayItem|_|) (index:int) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryItem index
+
+    let (|ArrayLast|_|) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryLast
+
+    let (|ArrayPick|_|) (chooser:'a -> 'b option) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Array.tryPick chooser
+
+    let (|ArrayTail|_|) (source:'a array) =
+        match source with
+        | null -> None
+        | _ ->
+            try
+                source
+                |> Array.tail
+                |> Some
+            with
+                | _ -> None
+
+    let (|ListContains|_|) (value:'a) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.contains value
+            |> BoolToOption
+
+    let (|ListExactlyOne|_|) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            try
+                source
+                |> List.exactlyOne
+                |> Some
+            with
+                | _ -> None
+
+    let (|ListExists|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.exists predicate
+            |> BoolToOption
+
+    let (|ListFind|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryFind predicate
+
+    let (|ListFindBack|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryFindBack predicate
+
+    let (|ListFindIndex|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryFindIndex predicate
+
+    let (|ListFindIndexBack|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryFindIndexBack predicate
+
+    let (|ListForAll|_|) (predicate:'a -> bool) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.forall predicate
+            |> BoolToOption
+
+    let (|ListItem|_|) (index:int) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryItem index
+
+    let (|ListLast|_|) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryLast
+
+    let (|ListPick|_|) (chooser:'a -> 'b option) (source:'a list) =
+        match source with
+        | [] -> None
+        | _ ->
+            source
+            |> List.tryPick chooser
+
+    let (|SeqContains|_|) (value:'a) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.contains value
+            |> BoolToOption
+
+    let (|SeqExactlyOne|_|) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            try
+                source
+                |> Seq.exactlyOne
+                |> Some
+            with
+                | _ -> None
+
+    let (|SeqExists|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.exists predicate
+            |> BoolToOption
+
+    let (|SeqFind|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryFind predicate
+
+    let (|SeqFindBack|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryFindBack predicate
+
+    let (|SeqFindIndex|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryFindIndex predicate
+
+    let (|SeqFindIndexBack|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryFindIndexBack predicate
+
+    let (|SeqForAll|_|) (predicate:'a -> bool) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.forall predicate
+            |> BoolToOption
+
+    let (|SeqHead|_|) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryHead
+
+    let (|SeqIsEmpty|_|) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.isEmpty
+            |> BoolToOption
+
+    let (|SeqItem|_|) (index:int) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryItem index
+
+    let (|SeqLast|_|) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryLast
+
+    let (|SeqPick|_|) (chooser:'a -> 'b option) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            source
+            |> Seq.tryPick chooser
+
+    let (|SeqTail|_|) (source:'a seq) =
+        match source with
+        | null -> None
+        | _ ->
+            try
+                source
+                |> Seq.tail
+                |> Some
+            with
+                | _ -> None
+
--- FSUtility/FSUtility/String.fs
+++ FSUtility/FSUtility/String.fs
@@ -2,8 +2,27 @@
 
 open System
 open System.Globalization
+open System.Text
 
 module String =
+
+    let (|Interned|_|) (str:string) =
+        match str with
+        | null -> None
+        | _ ->
+            str
+            |> String.IsInterned
+            |> Some
+
+    let (|NullOrEmpty|_|) (value:string) =
+        match String.IsNullOrEmpty(value) with
+        | true -> Some()
+        | false -> None
+
+    let (|NullOrWhiteSpace|_|) (value:string) =
+        match String.IsNullOrWhiteSpace(value) with
+        | true -> Some()
+        | false -> None
 
     let Slice (startIndex:int) (endIndex:int) (s:string):string = s.Substring(startIndex, endIndex - startIndex)
 
@@ -15,39 +34,66 @@ module String =
 
     let Insert (startIndex:int) (value:string) (s:string):string = s.Insert(startIndex, value)
 
-    let IsNormalized (s:string):bool = s.IsNormalized()
+    let IsNormalized (normalizationFormOption: NormalizationForm option) (s:string):bool =
+        match normalizationFormOption with
+        | None -> s.IsNormalized()
+        | Some(normalizationForm: NormalizationForm) -> s.IsNormalized(normalizationForm)
 
     let LastIndexOf (value:char) (s:string):int = s.LastIndexOf(value)
 
     let LastIndexOfAny (ofAny:char array) (s:string):int = s.LastIndexOfAny(ofAny)
 
-    let Normalize (s:string):string = s.Normalize()
+    let Normalize (normalizationFormOption: NormalizationForm option) (s:string):string =
+        match normalizationFormOption with
+        | None -> s.Normalize()
+        | Some(normalizationForm: NormalizationForm) -> s.Normalize(normalizationForm)
 
-    let PadLeft (totalWidth:int) (paddingChar:char) (s:string):string = s.PadLeft(totalWidth, paddingChar)
+    let PadLeft (totalWidth:int) (paddingCharOption:char option) (s:string):string =
+        match paddingCharOption with
+        | None -> s.PadLeft(totalWidth)
+        | Some(paddingChar: char) -> s.PadLeft(totalWidth, paddingChar)
 
-    let PadRight (totalWidth:int) (paddingChar:char) (s:string):string = s.PadRight(totalWidth, paddingChar)
+    let PadRight (totalWidth:int) (paddingCharOption:char option) (s:string):string =
+        match paddingCharOption with
+        | None -> s.PadRight(totalWidth)
+        | Some(paddingChar: char) -> s.PadRight(totalWidth, paddingChar)
 
-    let Remove (startIndex:int) (count:int) (s:string):string = s.Remove(startIndex, count)
+    let Remove (startIndex:int) (countOption:int option) (s:string):string =
+        match countOption with
+        | None -> s.Remove(startIndex)
+        | Some(count: int) -> s.Remove(startIndex, count)
 
     let Replace (oldValue:string) (newValue:string) (s:string):string = s.Replace(oldValue, newValue)
 
     let Split (separator:char array) (s:string):string array = s.Split(separator)
 
-    let Substring (startIndex:int) (length:int) (s:string):string = s.Substring(startIndex, length)
+    let Substring (startIndex:int) (lengthOption:int option) (s:string):string =
+        match lengthOption with
+        | None -> s.Substring(startIndex)
+        | Some(length: int) -> s.Substring(startIndex, length)
 
     let StartsWith (value:string) (s:string):bool = s.StartsWith(value)
 
     let ToCharArray (s:string):char array = s.ToCharArray()
 
-    let ToLower (culture:CultureInfo) (s:string):string = s.ToLower(culture)
+    let ToLower (cultureInfoOption:CultureInfo option) (s:string):string =
+        match cultureInfoOption with
+        | None -> s.ToLower()
+        | Some(cultureInfo: CultureInfo) -> s.ToLower(cultureInfo)
 
     let ToLowerInvariant (s:string):string = s.ToLowerInvariant()
 
-    let ToUpper (culture:CultureInfo) (s:string):string = s.ToUpper(culture)
+    let ToUpper (cultureInfoOption:CultureInfo option) (s:string):string =
+        match cultureInfoOption with
+        | None -> s.ToLower()
+        | Some(cultureInfo: CultureInfo) -> s.ToLower(cultureInfo)
 
     let ToUpperInvariant (s:string):string = s.ToUpperInvariant()
 
-    let Trim (trimChars:char array) (s:string):string = s.Trim(trimChars)
+    let Trim (trimCharsOption:char array option) (s:string):string =
+        match trimCharsOption with
+        | None -> s.Trim()
+        | Some(trimChars: char array) -> s.Trim(trimChars)
 
     let TrimEnd (trimChars:char array) (s:string):string = s.TrimEnd(trimChars)
 
