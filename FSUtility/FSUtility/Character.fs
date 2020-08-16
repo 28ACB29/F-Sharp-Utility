62c0e0ae0a82917dabdaa1838a01f9e41ce6082d
Arthur Baroi <arthurcbaroi@hotmail.com>
2020-08-16 06:57:40 -0500

    Extracted common functionality

--- FSUtility/FSUtility/Character.fs
+++ FSUtility/FSUtility/Character.fs
@@ -5,96 +5,38 @@ open System.Globalization
 
 module Character =
 
-    let (|Control|_|) (character:char) =
-        match Char.IsControl(character) with
+    let private yieldIfTrue (predicate:char -> bool) (character:char) =
+        match predicate character with
         | true ->
             character
             |> Some
         | false -> None
 
-    let (|Digit|_|) (character:char) =
-        match Char.IsDigit(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Control|_|) (character:char) = yieldIfTrue Char.IsControl character
 
-    let (|HighSurrogate|_|) (character:char) =
-        match Char.IsHighSurrogate(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Digit|_|) (character:char) = yieldIfTrue Char.IsDigit character
 
-    let (|Letter|_|) (character:char) =
-        match Char.IsLetter(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|HighSurrogate|_|) (character:char) = yieldIfTrue Char.IsHighSurrogate character
 
-    let (|LowSurrogate|_|) (character:char) =
-        match Char.IsLowSurrogate(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Letter|_|) (character:char) = yieldIfTrue Char.IsLetter character
 
-    let (|Lower|_|) (character:char) =
-        match Char.IsLower(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|LowSurrogate|_|) (character:char) = yieldIfTrue Char.IsLowSurrogate character
 
-    let (|Punctuation|_|) (character:char) =
-        match Char.IsPunctuation(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Lower|_|) (character:char) = yieldIfTrue Char.IsLower character
 
-    let (|Number|_|) (character:char) =
-        match Char.IsNumber(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Punctuation|_|) (character:char) = yieldIfTrue Char.IsPunctuation character
 
-    let (|Separator|_|) (character:char) =
-        match Char.IsSeparator(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Number|_|) (character:char) = yieldIfTrue Char.IsNumber character
 
-    let (|Surrogate|_|) (character:char) =
-        match Char.IsSurrogate(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Separator|_|) (character:char) = yieldIfTrue Char.IsSeparator character
 
-    let (|Symbol|_|) (character:char) =
-        match Char.IsSymbol(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Surrogate|_|) (character:char) = yieldIfTrue Char.IsSurrogate character
 
-    let (|Upper|_|) (character:char) =
-        match Char.IsUpper(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Symbol|_|) (character:char) = yieldIfTrue Char.IsSymbol character
 
-    let (|WhiteSpace|_|) (character:char) =
-        match Char.IsWhiteSpace(character) with
-        | true ->
-            character
-            |> Some
-        | false -> None
+    let (|Upper|_|) (character:char) = yieldIfTrue Char.IsUpper character
+
+    let (|WhiteSpace|_|) (character:char) = yieldIfTrue Char.IsWhiteSpace character
 
     let ToLower (culture:CultureInfo) (c:char):char = Char.ToLower(c, culture)
 
